8cea1a3100fee747eb4be571f632f1168561770d
Alexey Chernyshov <65975574+artifixer@users.noreply.github.com>
2020-08-06 13:01:11 +0300

    Added ClanHomeSettlementByRef

--- src/Bannerlord.ButterLib/CampaignIdentifier/Helpers/FieldAccessHelper.cs
+++ src/Bannerlord.ButterLib/CampaignIdentifier/Helpers/FieldAccessHelper.cs
@@ -1,5 +1,5 @@
-﻿using TaleWorlds.Localization;
-
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Localization;
 using static HarmonyLib.AccessTools;
 
 namespace Bannerlord.ButterLib.CampaignIdentifier.Helpers
@@ -7,5 +7,6 @@ namespace Bannerlord.ButterLib.CampaignIdentifier.Helpers
     internal static class FieldAccessHelper
     {
         internal static readonly FieldRef<TextObject, string> TextObjectValueByRef = FieldRefAccess<TextObject, string>("Value");
+        internal static readonly FieldRef<Clan, Settlement> ClanHomeSettlementByRef = FieldRefAccess<Clan, Settlement>("HomeSettlement");
     }
 }
