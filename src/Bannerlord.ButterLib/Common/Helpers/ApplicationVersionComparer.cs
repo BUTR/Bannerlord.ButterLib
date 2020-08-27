c627be271f61509cf4c496cc5db8991263b05f29
Vitaly Mikhailov <personal@aragas.org>
2020-08-27 20:54:32 +0300

    Fixed version comparer

---
+++ src/Bannerlord.ButterLib/Common/Helpers/ApplicationVersionComparer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+using TaleWorlds.Library;
+
+namespace Bannerlord.ButterLib.Common.Helpers
+{
+    public class ApplicationVersionComparer : IComparer<ApplicationVersion>
+    {
+        public int Compare(ApplicationVersion x, ApplicationVersion y)
+        {
+            var applicationVersionTypeComparison = x.ApplicationVersionType.CompareTo(y.ApplicationVersionType);
+            if (applicationVersionTypeComparison != 0) return applicationVersionTypeComparison;
+
+            var majorComparison = x.Major.CompareTo(y.Major);
+            if (majorComparison != 0) return majorComparison;
+
+            var minorComparison = x.Minor.CompareTo(y.Minor);
+            if (minorComparison != 0) return minorComparison;
+
+            var revisionComparison = x.Revision.CompareTo(y.Revision);
+            if (revisionComparison != 0) return revisionComparison;
+
+            var changeSetComparison = x.ChangeSet.CompareTo(y.ChangeSet);
+            if (changeSetComparison != 0) return changeSetComparison;
+
+            var versionGameTypeComparison = x.VersionGameType.CompareTo(y.VersionGameType);
+            if (versionGameTypeComparison != 0) return versionGameTypeComparison;
+
+            return 0;
+        }
+    }
+}
