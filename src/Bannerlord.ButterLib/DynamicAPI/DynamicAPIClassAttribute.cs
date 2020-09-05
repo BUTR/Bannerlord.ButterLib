7b230b813f399671801e4630e587bc6eb0ae6f3b
Vitaly Mikhailov <personal@aragas.org>
2020-09-06 00:00:05 +0300

    Init proposal

---
+++ src/Bannerlord.ButterLib/DynamicAPI/DynamicAPIClassAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bannerlord.ButterLib.DynamicAPI
+{
+    public class DynamicAPIClassAttribute : Attribute
+    {
+        public string Class { get; }
+
+        public DynamicAPIClassAttribute(string @class)
+        {
+            Class = @class;
+        }
+    }
+}
---
+++ src/Bannerlord.ButterLib/DynamicAPI/DynamicAPIMethodAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bannerlord.ButterLib.DynamicAPI
+{
+    public class DynamicAPIMethodAttribute : Attribute
+    {
+        public string Method { get; }
+
+        public DynamicAPIMethodAttribute(string method)
+        {
+            Method = method;
+        }
+    }
+}
