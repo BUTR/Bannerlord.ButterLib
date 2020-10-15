506b94036cc7353a6f403b53a8a109a337a3585c
Matthew D. Hall <zijistark@gmail.com>
2020-10-15 14:51:26 -0700

    concurrency-safe extension variables, collapsed mult-tier dicts

--- src/Bannerlord.ButterLib/SaveSystem/MBObjectBaseExtensionCampaignBehavior.cs
+++ src/Bannerlord.ButterLib/SaveSystem/MBObjectBaseExtensionCampaignBehavior.cs
@@ -1,27 +1,22 @@
 ﻿using Bannerlord.ButterLib.SaveSystem.Extensions;
-
-using System.Collections.Generic;
+using System;
+using System.Collections.Concurrent;
 
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.ObjectSystem;
 
 namespace Bannerlord.ButterLib.SaveSystem
 {
-    // TODO:
     internal sealed class MBObjectBaseExtensionCampaignBehavior : CampaignBehaviorBase
     {
         public static MBObjectBaseExtensionCampaignBehavior? Instance { get; set; }
-
-        private Dictionary<MBGUID, Dictionary<string, object?>>? _extensions;
 
         public override void SyncData(IDataStore dataStore)
         {
             dataStore.SyncDataAsJson("_descriptorManager", ref _extensions);
 
             if (dataStore.IsLoading && _extensions == null)
-            {
-                _extensions = new Dictionary<MBGUID, Dictionary<string, object?>>();
-            }
+                _extensions = new ConcurrentDictionary<StorageKey, object?>();
         }
 
         public override void RegisterEvents() { }
@@ -31,10 +26,7 @@ namespace Bannerlord.ButterLib.SaveSystem
             if (_extensions == null)
                 return;
 
-            if (!_extensions.TryGetValue(@object.Id, out _))
-                _extensions[@object.Id] = new Dictionary<string, object?>();
-
-            _extensions[@object.Id][key] = data;
+            _extensions[StorageKey.Make(@object, key)] = data;
         }
 
         public void RemoveExtension(MBObjectBase @object, string key)
@@ -42,11 +34,7 @@ namespace Bannerlord.ButterLib.SaveSystem
             if (_extensions == null)
                 return;
 
-            if (!_extensions.TryGetValue(@object.Id, out _))
-                _extensions[@object.Id] = new Dictionary<string, object?>();
-
-            if (_extensions[@object.Id].TryGetValue(key, out _))
-                _extensions[@object.Id].Remove(key);
+            _extensions.TryRemove(StorageKey.Make(@object, key), out _);
         }
 
 #nullable disable
@@ -55,14 +43,29 @@ namespace Bannerlord.ButterLib.SaveSystem
             if (_extensions == null)
                 return default;
 
-            if (!_extensions.TryGetValue(@object.Id, out _))
-                _extensions[@object.Id] = new Dictionary<string, object>();
-
-            if (_extensions[@object.Id].TryGetValue(key, out var value))
+            if (_extensions.TryGetValue(StorageKey.Make(@object, key), out var value))
                 return value;
 
             return default;
         }
 #nullable restore
+
+        class StorageKey : IEquatable<StorageKey>
+        {
+            public uint ObjectId;
+            public string Key;
+
+            public StorageKey(uint objectId, string key) => (ObjectId, Key) = (objectId, key);
+
+            public static StorageKey Make(MBObjectBase obj, string key) => new StorageKey(obj.Id.InternalValue, key);
+
+            public bool Equals(StorageKey other) => ObjectId == other.ObjectId && Key.Equals(other.Key);
+
+            public override bool Equals(object obj) => obj is StorageKey sk && Equals(sk);
+
+            public override int GetHashCode() => HashCode.Combine(ObjectId, Key);
+        }
+
+        private ConcurrentDictionary<StorageKey, object?>? _extensions;
     }
 }
