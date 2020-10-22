6bc9fe80bfa853c74256bca7e1b54854845a665e
Matthew D. Hall <zijistark@gmail.com>
2020-10-21 17:11:32 -0700

    added xml docs for MBObjectExtensions

--- src/Bannerlord.ButterLib/SaveSystem/Extensions/MBObjectBaseExtensions.cs
+++ src/Bannerlord.ButterLib/SaveSystem/Extensions/MBObjectBaseExtensions.cs
@@ -21,68 +21,111 @@ namespace Bannerlord.ButterLib.SaveSystem.Extensions
 
 #nullable disable
 
-        public static T GetVariable<T>(this MBObjectBase @object, string key)
+        /// <summary>
+        /// Get the value of the variable <paramref name="name"/> stored for <paramref name="object"/>
+        /// as a <typeparamref name="T"/>.
+        /// </summary>
+        /// <returns>
+        /// A value of type <typeparamref name="T"/> for the variable.
+        /// If no such variable exists, a default-valued <typeparamref name="T"/>.
+        /// </returns>
+        /// <example>
+        /// var marshal = myTown.GetVariable&lt;Hero&gt;("AppointedMarshal");
+        ///
+        /// if (marshal != null && marshal.IsAlive)
+        ///     UseMarshalForSomething(marshal);
+        /// </example>
+        /// <typeparam name="T">The type of the variable value.</typeparam>
+        /// <param name="object">A game object.</param>
+        /// <param name="name">The variable's name.</param>
+        public static T GetVariable<T>(this MBObjectBase @object, string name)
         {
             if (Instance is { } instance)
             {
                 if (typeof(T) == typeof(char))
                 {
-                    var str = instance.GetVariable<string>(@object, key);
+                    var str = instance.GetVariable<string>(@object, name);
                     return !string.IsNullOrEmpty(str) && str[0] is T val ? val : default;
                 }
 
-                return instance.GetVariable<T>(@object, key);
+                return instance.GetVariable<T>(@object, name);
             }
 
             return default;
         }
 
-        public static T GetVariableAsJson<T>(this MBObjectBase @object, string key, JsonSerializerSettings settings = null)
+        public static T GetVariableAsJson<T>(this MBObjectBase @object, string name, JsonSerializerSettings settings = null)
         {
-            if (Instance is { } instance && instance.GetVariable<string>(@object, key) is { } jsonData)
+            if (Instance is { } instance && instance.GetVariable<string>(@object, name) is { } jsonData)
                 return JsonConvert.DeserializeObject<T>(jsonData, settings);
             return default;
         }
 
 #nullable restore
 
-        public static void SetVariable<T>(this MBObjectBase @object, string key, T data)
+        /// <summary>
+        /// Set the value of the variable <paramref name="name"/> upon <paramref name="object"/>
+        /// to <paramref name="data"/>.
+        /// </summary>
+        /// <example>
+        /// myHero.SetVariable("Lovers", loverList);
+        /// </example>
+        /// <param name="object">A game object.</param>
+        /// <param name="name">The variable's name.</param>
+        public static void SetVariable<T>(this MBObjectBase @object, string name, T data)
         {
             if (Instance is { } instance)
             {
                 if (data is char @char)
                 {
-                    instance.SetVariable(@object, key, @char.ToString());
+                    instance.SetVariable(@object, name, @char.ToString());
                     return;
                 }
 
-                instance.SetVariable(@object, key, data);
+                instance.SetVariable(@object, name, data);
             }
         }
 
-        public static void SetVariableAsJson<T>(this MBObjectBase @object, string key, T data, JsonSerializerSettings? settings = null)
+        public static void SetVariableAsJson<T>(this MBObjectBase @object, string name, T data, JsonSerializerSettings? settings = null)
         {
             if (Instance is { } instance)
-                instance.SetVariable(@object, key, JsonConvert.SerializeObject(data, settings));
+                instance.SetVariable(@object, name, JsonConvert.SerializeObject(data, settings));
         }
 
-        public static void RemoveVariable(this MBObjectBase @object, string key)
+        /// <summary>
+        /// Remove the variable <paramref name="name"/> from <paramref name="object"/>.
+        /// </summary>
+        /// <param name="object">A game object.</param>
+        /// <param name="name">The variable's name.</param>
+        public static void RemoveVariable(this MBObjectBase @object, string name)
         {
             if (Instance is { } instance)
-                instance.RemoveVariable(@object, key);
+                instance.RemoveVariable(@object, name);
         }
 
         /* Flags */
 
+        /// <summary>
+        /// Check whether the flag <paramref name="name"/> is set upon <paramref name="object"/>.
+        /// </summary>
+        /// <returns><see langword="true"/> if the flag is set, else <see langword="false"/>.</returns>
+        /// <param name="object">A game object.</param>
+        /// <param name="name">A string flag.</param>
         public static bool HasFlag(MBObjectBase @object, string name) =>
             (Instance is { } instance) && instance.HasFlag(@object, name);
 
+        /// <summary>Set the flag <paramref name="name"/> upon <paramref name="object"/>.</summary>
+        /// <param name="object">A game object.</param>
+        /// <param name="name">A string flag.</param>
         public static void SetFlag(MBObjectBase @object, string name)
         {
             if (Instance is { } instance)
                 instance.SetFlag(@object, name);
         }
 
+        /// <summary>Remove the flag <paramref name="name"/> from <paramref name="object"/>, if set.</summary>
+        /// <param name="object">A game object.</param>
+        /// <param name="name">A string flag.</param>
         public static void RemoveFlag(MBObjectBase @object, string name)
         {
             if (Instance is { } instance)
