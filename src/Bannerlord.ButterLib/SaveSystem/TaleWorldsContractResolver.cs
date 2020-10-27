36cd017fecf49b67529612690fe1b8591b1a4471
Vitaly Mikhailov <personal@aragas.org>
2020-10-27 22:36:44 +0300

    Added base class to hold the fallbacks

--- src/Bannerlord.ButterLib/SaveSystem/TaleWorldsContractResolver.cs
+++ src/Bannerlord.ButterLib/SaveSystem/TaleWorldsContractResolver.cs
@@ -1,8 +1,4 @@
-﻿using Bannerlord.ButterLib.Common.Helpers;
-
-using HarmonyLib;
-
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 using System;
@@ -14,32 +10,8 @@ using TaleWorlds.SaveSystem;
 
 namespace Bannerlord.ButterLib.SaveSystem
 {
-    public class TaleWorldsContractResolver : DefaultContractResolver
+    public class TaleWorldsContractResolver : TaleWorldsContractResolverBase
     {
-        private delegate bool IsContainerDelegate(Type type);
-        private static readonly IsContainerDelegate? _isContainerDelegate =
-            AccessTools2.GetDelegate<IsContainerDelegate>(
-                AccessTools.Method(
-                    typeof(SaveableRootClassAttribute).Assembly.GetType("TaleWorlds.SaveSystem.TypeExtensions"), "IsContainer", new [] { typeof(Type) }));
-
-        private static bool IsContainerFallback(Type type)
-        {
-            if (type.IsGenericType && !type.IsGenericTypeDefinition)
-            {
-                var genericTypeDefinition = type.GetGenericTypeDefinition();
-                if (genericTypeDefinition == typeof(Dictionary<,>))
-                    return true;
-                if (genericTypeDefinition == typeof(List<>))
-                    return true;
-                if (genericTypeDefinition == typeof(Queue<>))
-                    return true;
-            }
-            else if (type.IsArray)
-                return true;
-
-            return false;
-        }
-
         protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
         {
             // Container are an exception.
---
+++ src/Bannerlord.ButterLib/SaveSystem/TaleWorldsContractResolverBase.cs
@@ -0,0 +1,40 @@
+﻿using Bannerlord.ButterLib.Common.Helpers;
+
+using HarmonyLib;
+
+using Newtonsoft.Json.Serialization;
+
+using System;
+using System.Collections.Generic;
+
+using TaleWorlds.SaveSystem;
+
+namespace Bannerlord.ButterLib.SaveSystem
+{
+    public class TaleWorldsContractResolverBase : DefaultContractResolver
+    {
+        protected delegate bool IsContainerDelegate(Type type);
+        protected static readonly IsContainerDelegate? _isContainerDelegate =
+            AccessTools2.GetDelegate<IsContainerDelegate>(
+                AccessTools.Method(
+                    typeof(SaveableRootClassAttribute).Assembly.GetType("TaleWorlds.SaveSystem.TypeExtensions"), "IsContainer", new [] { typeof(Type) }));
+
+        protected static bool IsContainerFallback(Type type)
+        {
+            if (type.IsGenericType && !type.IsGenericTypeDefinition)
+            {
+                var genericTypeDefinition = type.GetGenericTypeDefinition();
+                if (genericTypeDefinition == typeof(Dictionary<,>))
+                    return true;
+                if (genericTypeDefinition == typeof(List<>))
+                    return true;
+                if (genericTypeDefinition == typeof(Queue<>))
+                    return true;
+            }
+            else if (type.IsArray)
+                return true;
+
+            return false;
+        }
+    }
+}
