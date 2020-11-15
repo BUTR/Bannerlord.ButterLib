9486af7bc27bf7ab06f46e1a457204a990082a73
Matthew D. Hall <azrinon@gmail.com>
2020-11-15 01:50:43 -0800

    patch for `DefinitionContext.ConstructContainerDefinition`

--- src/Bannerlord.ButterLib.Implementation/SaveSystem/Patches/DefinitionContextPatch.cs
+++ src/Bannerlord.ButterLib.Implementation/SaveSystem/Patches/DefinitionContextPatch.cs
@@ -1,4 +1,5 @@
 ﻿using Bannerlord.ButterLib.Common.Extensions;
+using Bannerlord.ButterLib.Common.Helpers;
 
 using HarmonyLib;
 
@@ -42,40 +43,54 @@ namespace Bannerlord.ButterLib.Implementation.SaveSystem.Patches
             _log = ButterLibSubModule.Instance?.GetServiceProvider()?.GetRequiredService<ILogger<DefinitionContextPatch>>()
                    ?? NullLogger<DefinitionContextPatch>.Instance;
 
-            return Patches.Select(p => p.IsReady).All(ready => ready) && Patches.All(p => p.ApplyPrefix(harmony));
+            return Patches.Select(p => p.IsReady).All(ready => ready) && Patches.All(p => p.Apply(harmony));
         }
-
 
         // PATCH DEFINITIONS
 
         private class NoType { }
-        private static readonly Type? TargetType = typeof(MetaData).Assembly.GetType("TaleWorlds.SaveSystem.Definition.DefinitionContext");
+
+        private delegate bool IsContainerDelegate(Type type);
+        private static readonly Assembly TargetAssembly = typeof(ContainerType).Assembly;
+        private static readonly Type? TargetType = TargetAssembly.GetType("TaleWorlds.SaveSystem.Definition.DefinitionContext");
+        private static readonly Type? TypeExtensionsType = TargetAssembly.GetType("TaleWorlds.SaveSystem.TypeExtensions");
+        private static readonly MethodInfo? IsContainerMethod = AccessTools.Method(TypeExtensionsType ?? typeof(NoType), "IsContainer", new[] { typeof(Type) });
+        private static readonly IsContainerDelegate? IsContainer = AccessTools2.GetDelegate<IsContainerDelegate>(IsContainerMethod);
 
         private static MethodInfo? TargetTypeMethod(string name) => AccessTools.Method(TargetType ?? typeof(NoType), name);
 
-        private static readonly Patch[] Patches = new Patch[]
+        private sealed class ConstructContainerDefinitionPrefixPatch : PrefixPatch
         {
-            new("AddRootClassDefinitionPrefix",     TargetTypeMethod("AddRootClassDefinition")),
-            new("AddClassDefinitionPrefix",         TargetTypeMethod("AddClassDefinition")),
-            new("AddStructDefinitionPrefix",        TargetTypeMethod("AddStructDefinition")),
-            new("AddInterfaceDefinitionPrefix",     TargetTypeMethod("AddInterfaceDefinition")),
-            new("AddEnumDefinitionPrefix",          TargetTypeMethod("AddEnumDefinition")),
-            new("AddContainerDefinitionPrefix",     TargetTypeMethod("AddContainerDefinition")),
-            new("AddBasicTypeDefinitionPrefix",     TargetTypeMethod("AddBasicTypeDefinition")),
-            new("AddGenericClassDefinitionPrefix",  TargetTypeMethod("AddGenericClassDefinition")),
-            new("AddGenericStructDefinitionPrefix", TargetTypeMethod("AddGenericStructDefinition")),
+            internal ConstructContainerDefinitionPrefixPatch()
+                : base(nameof(ConstructContainerDefinitionPrefix), TargetTypeMethod("ConstructContainerDefinition")) { }
+
+            internal override bool IsReady => base.IsReady & ThisNotNull(IsContainer, $"{nameof(IsContainer)} delegate");
+        }
+
+        private static readonly Patch[] Patches = new PrefixPatch[]
+        {
+            new(nameof(AddBasicTypeDefinitionPrefix),     TargetTypeMethod("AddBasicTypeDefinition")),
+            new(nameof(AddClassDefinitionPrefix),         TargetTypeMethod("AddClassDefinition")),
+            new(nameof(AddStructDefinitionPrefix),        TargetTypeMethod("AddStructDefinition")),
+            new(nameof(AddInterfaceDefinitionPrefix),     TargetTypeMethod("AddInterfaceDefinition")),
+            new(nameof(AddEnumDefinitionPrefix),          TargetTypeMethod("AddEnumDefinition")),
+            new(nameof(AddRootClassDefinitionPrefix),     TargetTypeMethod("AddRootClassDefinition")),
+            new(nameof(AddGenericClassDefinitionPrefix),  TargetTypeMethod("AddGenericClassDefinition")),
+            new(nameof(AddGenericStructDefinitionPrefix), TargetTypeMethod("AddGenericStructDefinition")),
+            new(nameof(AddContainerDefinitionPrefix),     TargetTypeMethod("AddContainerDefinition")),
+            new ConstructContainerDefinitionPrefixPatch(),
         };
 
         // PATCH METHODS
 
         private static bool CanAddTypeDefinition(TypeDefinitionBase? typeDef, Dictionary<Type, TypeDefinitionBase> typeDict)
         {
-            if (typeDef is null)
+            if (typeDef is null || typeDef.Type is null)
                 return false;
 
             if (typeDict.ContainsKey(typeDef.Type))
             {
-                _log.LogTrace("Suppressed duplicate SaveSystem registration of type {type}", typeDef.Type.FullName);
+                _log.LogTrace("Suppressed duplicate definition of serializable type: {type}", typeDef.Type.FullName);
                 return false;
             }
 
@@ -118,26 +133,49 @@ namespace Bannerlord.ButterLib.Implementation.SaveSystem.Patches
                                                              Dictionary<Type, TypeDefinitionBase> ____allTypeDefinitions) =>
             CanAddTypeDefinition(genericStructDefinition, ____allTypeDefinitions);
 
-        // UTILITY
+        /*
+         * <summary>
+         * Prevent <c>DefinitionContext.ConstructContainerDefinition</c> from executing if the given
+         * <see cref="Type"> is not a supported container type.
+         * </summary>
+         * <remarks>
+         * The target method fails to check all used values of the <see cref="ContainerType"> enum;
+         * specifically, it does not check for the value <c>None</c>. Thus, it can easily add alleged
+         * container type definitions to the <c>DefinitionContext</c> whose fields are, besides the
+         * <see cref="Type">, entirely null. If this happens, the game will later crash.
+         * </remarks>
+         */
+        private static bool ConstructContainerDefinitionPrefix(Type type) => type is not null && IsContainer!(type);
 
-        private class Patch
+        // INSTRUMENTATION
+
+        private abstract class Patch
         {
-            internal readonly string      PatchMethodName;
+            internal readonly string PatchMethodName;
             internal readonly MethodInfo? PatchMethod;
             internal readonly MethodInfo? TargetMethod;
 
             internal Patch(string patchMethodName, MethodInfo? targetMethod)
             {
                 PatchMethodName = patchMethodName;
-                PatchMethod = AccessTools.Method(typeof(DefinitionContextPatch), patchMethodName);
+                PatchMethod = ResolvePatchMethod();
                 TargetMethod = targetMethod;
             }
 
-            internal bool ApplyPrefix(Harmony harmony) => harmony.Patch(TargetMethod, prefix: new HarmonyMethod(PatchMethod)) is not null;
+            internal abstract bool Apply(Harmony harmony);
 
-            internal bool IsReady => MethodNotNull(PatchMethod, nameof(PatchMethod)) & MethodNotNull(TargetMethod, nameof(TargetMethod));
+            internal virtual bool IsReady => ThisNotNull(PatchMethod, nameof(PatchMethod)) & ThisNotNull(TargetMethod, nameof(TargetMethod));
 
-            private bool MethodNotNull(MethodInfo? method, string methodName) => NotNull(method, methodName, $"Patch {PatchMethodName}: ");
+            protected virtual MethodInfo ResolvePatchMethod() => AccessTools.Method(GetType().DeclaringType, PatchMethodName);
+
+            protected bool ThisNotNull(object? obj, string objName) => NotNull(obj, objName, $"Patch {PatchMethodName}: ");
+        }
+
+        private class PrefixPatch : Patch
+        {
+            internal PrefixPatch(string patchMethodName, MethodInfo? targetMethod) : base(patchMethodName, targetMethod) { }
+
+            internal override bool Apply(Harmony harmony) => harmony.Patch(TargetMethod, prefix: new HarmonyMethod(PatchMethod)) is not null;
         }
 
         private static bool NotNull<T>(T obj, string name, string? errPrefix = null) where T : class?
