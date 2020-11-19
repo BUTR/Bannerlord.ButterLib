8184a6a76f448e9e24804c8dd96edda14e24ddb6
Matthew D. Hall <azrinon@gmail.com>
2020-11-19 14:36:00 -0800

    commenting out patches that should be tested before a release

--- src/Bannerlord.ButterLib.Implementation/SubModule.cs
+++ src/Bannerlord.ButterLib.Implementation/SubModule.cs
@@ -82,7 +82,7 @@ namespace Bannerlord.ButterLib.Implementation
 
                 var saveSystemHarmony = new Harmony("Bannerlord.ButterLib.SaveSystem");
                 TypeExtensionsPatch.Apply(saveSystemHarmony); // Adds support for saving many more container types
-                DefinitionContextPatch.Apply(saveSystemHarmony); // Fixes save corruption & crashes when duplicate types are defined
+                //DefinitionContextPatch.Apply(saveSystemHarmony); // Fixes save corruption & crashes when duplicate types are defined
             }
 
             Logger.LogTrace("ButterLib.Implementation: OnBeforeInitialModuleScreenSetAsRoot: Done");
