7a57bacd01cfc63068da11c842a4c3b3e34fcddf
Vitaly Mikhailov <personal@aragas.org>
2020-12-01 22:16:33 +0300

    Added ModuleSorter

--- src/Bannerlord.ButterLib/Common/Helpers/ExtendedModuleInfo.cs
+++ src/Bannerlord.ButterLib/Common/Helpers/ExtendedModuleInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
 
@@ -7,12 +8,38 @@ using TaleWorlds.Library;
 
 namespace Bannerlord.ButterLib.Common.Helpers
 {
+    public enum LoadType { LoadAfterThis, LoadBeforeThis }
+
+    public readonly struct DependedModuleMetadata
+    {
+        public readonly string Id;
+        public readonly LoadType LoadType;
+        public readonly bool IsOptional;
+
+        public DependedModuleMetadata(string id, LoadType loadType, bool isOptional)
+        {
+            Id = id;
+            LoadType = loadType;
+            IsOptional = isOptional;
+        }
+
+        public override bool Equals(object obj) => obj is DependedModuleMetadata objStr && Equals(objStr);
+        public bool Equals(DependedModuleMetadata obj) => Id.Equals(obj.Id) && LoadType.Equals(obj.LoadType) && IsOptional.Equals(obj.IsOptional);
+
+        public override int GetHashCode() => HashCode.Combine(Id, LoadType, IsOptional);
+
+        public static bool operator ==(DependedModuleMetadata left, DependedModuleMetadata right) => left.Equals(right);
+        public static bool operator !=(DependedModuleMetadata left, DependedModuleMetadata right) => !(left == right);
+    }
+
     public sealed class ExtendedModuleInfo : ModuleInfo
     {
         public string Url { get; private set; } = string.Empty;
         public List<ExtendedSubModuleInfo> ExtendedSubModules { get; } = new List<ExtendedSubModuleInfo>();
         public string XmlPath { get; private set; } = string.Empty;
         public string Folder { get; private set; } = string.Empty;
+
+        public readonly List<DependedModuleMetadata> DependedModuleMetadatas = new List<DependedModuleMetadata>();
 
         public new void Load(string alias)
         {
@@ -29,15 +56,28 @@ namespace Bannerlord.ButterLib.Common.Helpers
             Url = xmlNodeModule?.SelectSingleNode("Url")?.Attributes?["value"]?.InnerText ?? string.Empty;
 
             var xmlNodeSubModules = xmlNodeModule?.SelectSingleNode("SubModules");
-            foreach (var xmlNodeSubModule in xmlNodeSubModules?.OfType<XmlElement>() ?? Enumerable.Empty<XmlElement>())
+            foreach (var xmlElement in xmlNodeSubModules?.OfType<XmlElement>() ?? Enumerable.Empty<XmlElement>())
             {
                 var subModuleInfo = new ExtendedSubModuleInfo();
-                subModuleInfo.LoadFrom(xmlNodeSubModule, System.IO.Path.Combine(Utilities.GetBasePath(), "Modules", alias));
+                subModuleInfo.LoadFrom(xmlElement, System.IO.Path.Combine(Utilities.GetBasePath(), "Modules", alias));
                 ExtendedSubModules.Add(subModuleInfo);
             }
 
             XmlPath = GetPath(Alias);
             Folder = System.IO.Path.GetDirectoryName(XmlPath)!;
+
+            DependedModuleMetadatas.Clear();
+            var xmlNodeDependedModuleMetadatas = xmlNodeModule?.SelectSingleNode("DependedModuleMetadatas");
+            foreach (var xmlElement in xmlNodeDependedModuleMetadatas?.OfType<XmlElement>() ?? Enumerable.Empty<XmlElement>())
+            {
+                if (xmlElement.Attributes["id"] is { } idAttr && xmlElement.Attributes["order"] is { } orderAttr && Enum.TryParse<LoadType>(orderAttr.InnerText, out var order))
+                {
+                    var optional = xmlElement.Attributes["optional"]?.InnerText.Equals("true") ?? false;
+                    DependedModuleMetadatas.Add(new DependedModuleMetadata(idAttr.InnerText, order, optional));
+                }
+            }
         }
+
+        public override string ToString() => $"{Id} - {Version}";
     }
 }
