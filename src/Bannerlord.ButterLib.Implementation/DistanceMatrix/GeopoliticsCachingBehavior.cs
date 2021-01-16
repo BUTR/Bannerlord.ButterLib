6ca04cc3247abe804b7101fe8a5b0f4f5d73255c
Vitaly Mikhailov <personal@aragas.org>
2021-01-16 18:34:49 +0300

    Build fix

--- src/Bannerlord.ButterLib.Implementation/DistanceMatrix/GeopoliticsCachingBehavior.cs
+++ src/Bannerlord.ButterLib.Implementation/DistanceMatrix/GeopoliticsCachingBehavior.cs
@@ -48,7 +48,7 @@ namespace Bannerlord.ButterLib.Implementation.DistanceMatrix
 
             if ((newOwner.Clan is not null || oldOwner.Clan is not null) && newOwner.Clan != oldOwner.Clan)
             {
-                var clans = Clan.All.Where(c => c.IsInitialized && c.Fiefs.Count > 0).ToList();
+                var clans = Clan.All.Where(c => c.IsInitialized && c.Fiefs.Any()).ToList();
 
                 if (oldOwner.Clan is not null)
                 {
