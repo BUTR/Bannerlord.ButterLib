e4edcec06ffbfb2d0b15f6abebe5e72de9340fda
Vitaly Mikhailov <personal@aragas.org>
2021-06-12 17:40:32 +0300

    ButterLib should handle more game exceptions now

--- src/Bannerlord.ButterLib/ExceptionHandler/BEWPatch.cs
+++ src/Bannerlord.ButterLib/ExceptionHandler/BEWPatch.cs
@@ -1,16 +1,15 @@
 ﻿using HarmonyLib;
+using HarmonyLib.BUTR.Extensions;
 
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 using TaleWorlds.DotNet;
 using TaleWorlds.Engine;
 using TaleWorlds.Engine.Screens;
 using TaleWorlds.MountAndBlade;
-using TaleWorlds.MountAndBlade.View.Missions;
-
-using static HarmonyLib.AccessTools;
 
 namespace Bannerlord.ButterLib.ExceptionHandler
 {
@@ -18,14 +17,11 @@ namespace Bannerlord.ButterLib.ExceptionHandler
     {
         internal static readonly HashSet<Exception> SuppressedExceptions = new();
 
-        private static readonly MethodInfo? ManagedApplicationTickMethod = Method(typeof(Managed), "ApplicationTick");
-        private static readonly MethodInfo? ScriptComponentBehaviourOnTickMethod = Method(typeof(ScriptComponentBehaviour), "OnTick");
-        private static readonly MethodInfo? ScreenManagerTickMethod = Method(typeof(ScreenManager), "Tick");
-        private static readonly MethodInfo? ModuleOnApplicationTickMethod = Method(typeof(TaleWorlds.MountAndBlade.Module), "OnApplicationTick");
-        private static readonly MethodInfo? MissionTickMethod = Method(typeof(Mission), "Tick");
-        private static readonly MethodInfo? MissionBehaviourTickMethod = Method(typeof(MissionBehaviour), "OnMissionTick");
-        private static readonly MethodInfo? MissionViewOnMissionScreenTickMethod = Method(typeof(MissionView), "OnMissionScreenTick");
-        private static readonly MethodInfo? FinalizerMethod = Method(typeof(BEWPatch), nameof(Finalizer));
+        private static readonly MethodInfo? ManagedApplicationTickMethod = AccessTools2.Method(typeof(Managed), "ApplicationTick");
+        private static readonly MethodInfo? ScreenManagerTickMethod = AccessTools2.Method(typeof(ScreenManager), "Tick");
+        private static readonly MethodInfo? ManagedScriptHolderTickComponentsMethod = AccessTools2.Method(typeof(ManagedScriptHolder), "TickComponents");
+        private static readonly MethodInfo? MissionTickMethod = AccessTools2.Method(typeof(Mission), "Tick");
+        private static readonly MethodInfo? FinalizerMethod = AccessTools2.Method(typeof(BEWPatch), nameof(Finalizer));
 
         private static void Finalizer(Exception? __exception)
         {
@@ -35,35 +31,49 @@ namespace Bannerlord.ButterLib.ExceptionHandler
                 HtmlBuilder.BuildAndShow(new CrashReport(__exception));
             }
         }
-        private static void FinalizerSuppress(ref Exception? __exception)
-        {
-            __exception = null;
-        }
 
         internal static void Enable(Harmony harmony)
         {
-            var array = new [] { "org.calradia.admiralnelson.betterexceptionwindow" };
+            var array = new[] { "org.calradia.admiralnelson.betterexceptionwindow" };
 
             harmony.Patch(ManagedApplicationTickMethod, finalizer: new HarmonyMethod(FinalizerMethod, before: array));
-            harmony.Patch(ScriptComponentBehaviourOnTickMethod, finalizer: new HarmonyMethod(FinalizerMethod, before: array));
             harmony.Patch(ScreenManagerTickMethod, finalizer: new HarmonyMethod(FinalizerMethod, before: array));
-
-            harmony.Patch(ModuleOnApplicationTickMethod, finalizer: new HarmonyMethod(FinalizerMethod, before: array));
+            harmony.Patch(ManagedScriptHolderTickComponentsMethod, finalizer: new HarmonyMethod(FinalizerMethod, before: array));
             harmony.Patch(MissionTickMethod, finalizer: new HarmonyMethod(FinalizerMethod, before: array));
-            harmony.Patch(MissionBehaviourTickMethod, finalizer: new HarmonyMethod(FinalizerMethod, before: array));
-            harmony.Patch(MissionViewOnMissionScreenTickMethod, finalizer: new HarmonyMethod(FinalizerMethod, before: array));
+
+            // Managed.ApplicationTick
+            harmony.TryPatch(
+                AccessTools2.Method("ManagedCallbacks.LibraryCallbacksGenerated:Managed_ApplicationTick"),
+                transpiler: AccessTools2.Method(typeof(BEWPatch), nameof(BlankTranspiler)));
+            // ScreenManager.Tick
+            harmony.TryPatch(
+                AccessTools2.Method("ManagedCallbacks.EngineCallbacksGenerated.ScreenManager_Tick"),
+                transpiler: AccessTools2.Method(typeof(BEWPatch), nameof(BlankTranspiler)));
+            // ManagedScriptHolder.TickComponents
+            harmony.TryPatch(
+                AccessTools2.Method("ManagedCallbacks.EngineCallbacksGenerated.ManagedScriptHolder_TickComponents"),
+                transpiler: AccessTools2.Method(typeof(BEWPatch), nameof(BlankTranspiler)));
+            // Mission.Tick
+            harmony.TryPatch(
+                AccessTools2.Method("TaleWorlds.MountAndBlade.MissionState.FinishMissionLoading"),
+                transpiler: AccessTools2.Method(typeof(BEWPatch), nameof(BlankTranspiler)));
+            harmony.TryPatch(
+                AccessTools2.Method("TaleWorlds.MountAndBlade.MissionState.TickMissionAux"),
+                transpiler: AccessTools2.Method(typeof(BEWPatch), nameof(BlankTranspiler)));
+            harmony.TryPatch(
+                AccessTools2.Method("TaleWorlds.MountAndBlade.MissionState.TickMission"),
+                transpiler: AccessTools2.Method(typeof(BEWPatch), nameof(BlankTranspiler)));
         }
 
         internal static void Disable(Harmony harmony)
         {
             harmony.Unpatch(ManagedApplicationTickMethod, FinalizerMethod);
-            harmony.Unpatch(ScriptComponentBehaviourOnTickMethod, FinalizerMethod);
             harmony.Unpatch(ScreenManagerTickMethod, FinalizerMethod);
-
-            harmony.Unpatch(ModuleOnApplicationTickMethod, FinalizerMethod);
+            harmony.Unpatch(ManagedScriptHolderTickComponentsMethod, FinalizerMethod);
             harmony.Unpatch(MissionTickMethod, FinalizerMethod);
-            harmony.Unpatch(MissionBehaviourTickMethod, FinalizerMethod);
-            harmony.Unpatch(MissionViewOnMissionScreenTickMethod, FinalizerMethod);
         }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static IEnumerable<CodeInstruction> BlankTranspiler(IEnumerable<CodeInstruction> instructions) => instructions;
     }
 }
