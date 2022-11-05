5497cce16f7f9f7b69911efe57461482460eda09
Aragas <personal@aragas.org>
2022-11-05 17:00:45 +0200

    * Removed unnecessary collision for HotKeys
    * Better explanation of why a crash report failed to upload

--- src/Bannerlord.ButterLib/CrashUploader/BUTRCrashUploader.cs
+++ src/Bannerlord.ButterLib/CrashUploader/BUTRCrashUploader.cs
@@ -1,5 +1,6 @@
 ﻿using Bannerlord.ButterLib.ExceptionHandler;
 
+using System;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -11,33 +12,47 @@ namespace Bannerlord.ButterLib.CrashUploader
 {
     internal class BUTRCrashUploader : ICrashUploader
     {
-        public async Task<string?> UploadAsync(CrashReport crashReport)
+        public async Task<CrashUploaderResult> UploadAsync(CrashReport crashReport)
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            var uploadUrlAttr = assembly.GetCustomAttributes<AssemblyMetadataAttribute>().FirstOrDefault(a => a.Key == "BUTRUploadUrl");
-            if (uploadUrlAttr is null)
-                return null;
-
-            // Do not send a minidump
-            var htmlReport = HtmlBuilder.Build(crashReport, string.Empty);
-            var data = Encoding.UTF8.GetBytes(htmlReport);
-
-            var httpWebRequest = WebRequest.CreateHttp($"{uploadUrlAttr.Value}");
-            httpWebRequest.Method = "POST";
-            httpWebRequest.ContentType = "text/html";
-            httpWebRequest.ContentLength = data.Length;
-            httpWebRequest.UserAgent = $"ButterLib CrashUploader v{assembly.GetName().Version}";
-
-            using var requestStream = await httpWebRequest.GetRequestStreamAsync().ConfigureAwait(false);
-            await requestStream.WriteAsync(data, 0, data.Length).ConfigureAwait(false);
-
-            if (await httpWebRequest.GetResponseAsync().ConfigureAwait(false) is HttpWebResponse { StatusCode: HttpStatusCode.OK } response && response.GetResponseStream() is { } stream)
+            try
             {
-                using var responseReader = new StreamReader(stream);
-                return await responseReader.ReadLineAsync().ConfigureAwait(false);
-            }
+                var assembly = Assembly.GetExecutingAssembly();
+                var uploadUrlAttr = assembly.GetCustomAttributes<AssemblyMetadataAttribute>().FirstOrDefault(a => a.Key == "BUTRUploadUrl");
+                if (uploadUrlAttr is null)
+                    return CrashUploaderResult.MetadataNotFound();
 
-            return null;
+                // Do not send a minidump
+                var htmlReport = HtmlBuilder.Build(crashReport, string.Empty);
+                var data = Encoding.UTF8.GetBytes(htmlReport);
+
+                var httpWebRequest = WebRequest.CreateHttp($"{uploadUrlAttr.Value}");
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentType = "text/html";
+                httpWebRequest.ContentLength = data.Length;
+                httpWebRequest.UserAgent = $"ButterLib CrashUploader v{assembly.GetName().Version}";
+
+                using var requestStream = await httpWebRequest.GetRequestStreamAsync().ConfigureAwait(false);
+                await requestStream.WriteAsync(data, 0, data.Length).ConfigureAwait(false);
+
+                using var response = await httpWebRequest.GetResponseAsync().ConfigureAwait(false);
+                if (response is not HttpWebResponse httpWebResponse)
+                    return CrashUploaderResult.ResponseIsNotHttpWebResponse();
+
+                if (httpWebResponse.StatusCode != HttpStatusCode.OK)
+                    return CrashUploaderResult.WrongStatusCode(httpWebResponse.StatusCode.ToString());
+
+                using var stream = response.GetResponseStream();
+                if (stream is null)
+                    return CrashUploaderResult.ResponseStreamIsNull();
+
+                using var responseReader = new StreamReader(stream);
+                var result = await responseReader.ReadLineAsync().ConfigureAwait(false);
+                return CrashUploaderResult.Success(result);
+            }
+            catch (Exception e)
+            {
+                return CrashUploaderResult.FailedWithException(e.ToString());
+            }
         }
     }
 }
---
+++ src/Bannerlord.ButterLib/CrashUploader/CrashUploaderResult.cs
@@ -0,0 +1,22 @@
+﻿namespace Bannerlord.ButterLib.CrashUploader
+{
+    internal record CrashUploaderResult
+    {
+        public static CrashUploaderResult Success(string url) => new(CrashUploaderStatus.Success) { Url = url};
+        public static CrashUploaderResult MetadataNotFound() => new(CrashUploaderStatus.MetadataNotFound);
+        public static CrashUploaderResult ResponseIsNotHttpWebResponse() => new(CrashUploaderStatus.ResponseIsNotHttpWebResponse);
+        public static CrashUploaderResult WrongStatusCode(string statusCode) => new(CrashUploaderStatus.WrongStatusCode) { StatusCode = statusCode };
+        public static CrashUploaderResult ResponseStreamIsNull() => new(CrashUploaderStatus.ResponseStreamIsNull);
+        public static CrashUploaderResult FailedWithException(string exception) => new(CrashUploaderStatus.FailedWithException) { Exception = exception };
+
+        public CrashUploaderStatus Status { get; }
+        public string? Url { get; private init; }
+        public string? StatusCode { get; private init; }
+        public string? Exception { get; private init; }
+
+        private CrashUploaderResult(CrashUploaderStatus status)
+        {
+            Status = status;
+        }
+    }
+}
