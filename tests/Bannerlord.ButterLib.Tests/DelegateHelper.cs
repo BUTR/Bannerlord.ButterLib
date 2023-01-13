0b519e533ad0e7af7c35eb4c04bec240bd9de44b
Aragas <personal@aragas.org>
2023-01-13 23:05:01 +0200

    Fixed tests

--- tests/Bannerlord.ButterLib.Tests/DelegateHelper.cs
+++ tests/Bannerlord.ButterLib.Tests/DelegateHelper.cs
@@ -1,4 +1,5 @@
-﻿using Bannerlord.ModuleManager;
+﻿using Bannerlord.BUTR.Shared.Helpers;
+using Bannerlord.ModuleManager;
 
 using System;
 using System.Collections.Generic;
@@ -8,13 +9,13 @@ namespace Bannerlord.ButterLib.Tests
 {
     internal static class DelegateHelper
     {
-        public delegate bool MockedGetModuleInfoDelegate(Type type, ref object? __result);
+        public delegate bool MockedGetModuleInfoDelegate(Type type, ref ModuleInfoExtendedWithMetadata? __result);
         public static MethodInfo GetMethodInfo(MockedGetModuleInfoDelegate @delegate) => @delegate.Method;
 
         public delegate bool MockedGetBasePathDelegate(ref string __result);
         public static MethodInfo GetMethodInfo(this MockedGetBasePathDelegate @delegate) => @delegate.Method;
 
-        public delegate bool MockedGetLoadedModulesDelegate(ref IEnumerable<ModuleInfoExtended> __result);
+        public delegate bool MockedGetLoadedModulesDelegate(ref IEnumerable<ModuleInfoExtendedWithMetadata> __result);
         public static MethodInfo GetMethodInfo(MockedGetLoadedModulesDelegate @delegate) => @delegate.Method;
     }
 }
